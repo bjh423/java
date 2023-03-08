51c6f2055024765a5e6a28232dd883fef2324e24
bjh423 <qkdwnsgh0423@gmail.com>
2023-03-08 10:38:15 +0900

    230308 commit2 가입,탈퇴,수정 시 return값 설정 return값을 통해 화면에 실행결과 출력

--- jdbc01/src/자바DB연결/MemberDao2.java
+++ jdbc01/src/자바DB연결/MemberDao2.java
@@ -6,7 +6,8 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class MemberDao2 {
-	public void insert(String id, String pw, String name, String tel) {
+	public int insert(String id, String pw, String name, String tel) {
+		int result = 0;
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			System.out.println("1. 드라이버 설정 성공");
@@ -27,16 +28,25 @@ public class MemberDao2 {
 
 			System.out.println("3. sql문 부품(객체)로 만들기");
 
-			ps.executeUpdate();
-			System.out.println("4. sql문 전송 성공");
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+			result = ps.executeUpdate();
 
+			System.out.println("4. sql문 전송 성공");
+
+			if (result == 1) {
+				System.out.println("회원가입 성공");
+			}
+
+		} catch (Exception e) {
+			// insert가 실패한 경우, 실행
+			result = 0;
+			// e.printStackTrace();
+		}
+		System.out.println(result);
+		return result;
 	}
 
-	public void delete(String id) {
+	public int delete(String id) {
+		int result = 0;
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			System.out.println("1. 드라이버 설정 성공");
@@ -52,15 +62,21 @@ public class MemberDao2 {
 			ps.setString(1, id);
 			System.out.println("3. sql문 부품(객체)로 만들기");
 
-			ps.executeUpdate();
+			result = ps.executeUpdate();
+			if (result == 1) {
+				System.out.println("회원탈퇴 성공");
+			}
 			System.out.println("4. sql문 전송 성공");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			result = 0;
+			// e.printStackTrace();
 		}
+		return result;
 	}
 
-	public void update(String id, String tel) {
+	public int update(String id, String tel) {
+		int result = 0;
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			System.out.println("1. 드라이버 설정 성공");
@@ -77,12 +93,17 @@ public class MemberDao2 {
 			ps.setString(2, id);
 			System.out.println("3. sql문 부품(객체)로 만들기");
 
-			ps.executeUpdate();
+			result = ps.executeUpdate();
+			if (result == 1) {
+				System.out.println("회원정보 수정 성공");
+			}
 			System.out.println("4. sql문 전송 성공");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			result = 0;
 		}
+		return result;
 	}
 
 }
